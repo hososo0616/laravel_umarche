6fe1de3ff59b0702a6558ed0196866ea6d99345c
hososo <hoso6616@gmail.com>
2023-04-01 06:54:48 +0900

    セクション２終了

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー１</x-slot>
     コンポーネントテスト１
+
+    <x-tests.card title="タイトル" content="本文" :message="$message"/>
+    <x-tests.card title="タイトル" />
+    <x-tests.card title="css変えたい" class="bg-red-300"/>
 </x-tests.app>
