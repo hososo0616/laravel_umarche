87936ed89bff4239c1003e2f851595de948a1de6
hososo <hoso6616@gmail.com>
2023-04-22 22:06:52 +0900

    オーナー機能実装完了

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
+use App\Models\Product;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
 use Illuminate\Support\Facades\Storage;
@@ -39,8 +40,8 @@ class ImageController extends Controller
     public function index()
     {
         $images = Image::where('owner_id', Auth::id())
-        ->orderBy('updated_at', 'desc')
-        ->paginate(20);
+            ->orderBy('updated_at', 'desc')
+            ->paginate(20);
 
         return view('owner.images.index', compact('images'));
     }
@@ -75,11 +76,11 @@ class ImageController extends Controller
         }
 
         return redirect()
-        ->route('owner.images.index')
-        ->with([
-            'message' => '画像登録をしました。',
-            'status' => 'info'
-        ]);
+            ->route('owner.images.index')
+            ->with([
+                'message' => '画像登録をしました。',
+                'status' => 'info'
+            ]);
     }
 
     /**
@@ -112,11 +113,11 @@ class ImageController extends Controller
         $image->save();
 
         return redirect()
-        ->route('owner.images.index')
-        ->with([
-            'message' => '画像情報を更新しました。',
-            'status' => 'info'
-        ]);
+            ->route('owner.images.index')
+            ->with([
+                'message' => '画像情報を更新しました。',
+                'status' => 'info'
+            ]);
     }
 
     /**
@@ -128,7 +129,37 @@ class ImageController extends Controller
     public function destroy($id)
     {
         $image = Image::findOrFail($id);
+
+        $imageInProducts = Product::where('image1', $image->id)
+            ->orWhere('image2', $image->id)
+            ->orWhere('image3', $image->id)
+            ->orWhere('image4', $image->id)
+            ->get();
+
+        if ($imageInProducts) {
+            $imageInProducts->each(function ($product) use ($image) {
+                if ($product->image1 === $image->id) {
+                    $product->image1 = null;
+                    $product->save();
+                }
+                if ($product->image2 === $image->id) {
+                    $product->image2 = null;
+                    $product->save();
+                }
+                if ($product->image3 === $image->id) {
+                    $product->image3 = null;
+                    $product->save();
+                }
+                if ($product->image4 === $image->id) {
+                    $product->image4 = null;
+                    $product->save();
+                }
+            });
+        }
+
         $filePath = 'public/products/' . $image->filename;
+
+
 
         if (Storage::exists($filePath)) {
             Storage::delete($filePath);
